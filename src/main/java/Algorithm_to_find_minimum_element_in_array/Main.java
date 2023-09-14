e55cc78028425e1bac30bb4104f0a7386a92b383
Kvazar103 <nazar.voinarovych250@gmail.com>
2023-09-14 22:56:30 +0300

    init

--- src/main/java/Algorithm_to_find_minimum_element_in_array/Main.java
+++ src/main/java/Algorithm_to_find_minimum_element_in_array/Main.java
@@ -1,2 +1,20 @@
-package Algorithm_to_find_minimum_element_in_array;public class Main {
+package Algorithm_to_find_minimum_element_in_array;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array=new int[] {64,12,14,29,80,20,57,19,10,105,78};
+
+        int minElement=array[0];
+        int minIndex=0;
+
+        for (int i = 1; i <array.length ; i++) {
+            if(array[i]<minElement){
+                minElement=array[i];
+                minIndex=i;
+            }
+        }
+        System.out.println("Min value: "+minElement);
+        System.out.println("Index of min value: "+minIndex);
+
+    }
 }
--- src/main/java/Bubble_sort/Main.java
+++ src/main/java/Bubble_sort/Main.java
@@ -1,2 +1,38 @@
-package Bubble_sort;public class Main {
+package Bubble_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = new int[]{64, 12, 14, 29, 80, 20, 57, 19, 10, 105, 78};
+
+        System.out.println(Arrays.toString(array));
+        boolean isSorted = false;
+
+        while (!isSorted) { //isSorted!=true
+            isSorted=true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted=false;// Це означає, що ще потрібно провести щонайменше ще одну ітерацію, оскільки масив не впорядкований.
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+//метод для виведення масиву
+//    private static void printArray(int[] arr) {
+//        System.out.print("[");
+//        for (int i = 0; i < arr.length; i++) {
+//            if (i > 0) {
+//                System.out.print(", ");
+//            }
+//            System.out.print(arr[i]);
+//        }
+//        System.out.print("]");
+//    }
 }
